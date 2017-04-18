276263a4fd7fc665626184d17a8a60abdffce480
ELCR04-PC12 <ELCR04-PC12>
2017-04-18 16:42:32 -0300

    Braulio Briones 18-04-2017 16:42

---
+++ ejemploMartes/app/src/main/java/com/example/rlcr/ejemplomartes/BraulioBriones.java
@@ -0,0 +1,13 @@
+package com.example.rlcr.ejemplomartes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BraulioBriones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_braulio_briones);
+    }
+}
--- ejemploMartes/app/src/main/java/com/example/rlcr/ejemplomartes/MainActivity.java
+++ ejemploMartes/app/src/main/java/com/example/rlcr/ejemplomartes/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.rlcr.ejemplomartes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button braulio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        braulio = (Button) findViewById(R.id.btn_braulio);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        braulio.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent braulioActivity = new Intent(MainActivity.this, BraulioBriones.class);
+            }
+        });
     }
 }
